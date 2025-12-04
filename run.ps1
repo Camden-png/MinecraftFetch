8cb9b84dbf3736c11c691c5a501a99695596d1a2
Camden Pettijohn <camdenpettijohn@gmail.com>
2025-12-03 22:56:47 -0800

    Updated run.ps1

--- run.ps1
+++ run.ps1
@@ -1,2 +1,14 @@
-java -Xmx512M -Xms512M -jar -DIReallyKnowWhatIAmDoingISwear paper.jar nogui
-Pause
+Write-Host "Running" -NoNewline
+for ($i = 1; $i -le 3; $i++) {
+    Start-Sleep -Seconds 0.1
+    Write-Host "." -NoNewline
+}
+Write-Host ""  # Print newline...
+
+try {
+    # Do not log with ANSI escape sequences & Minecraft color codes...
+    java -Xmx512M -Xms512M -DIReallyKnowWhatIAmDoingISwear "-Dterminal.ansi=false" -jar paper.jar nogui --log-strip-color > server.log 2>&1
+}
+finally {
+    Write-Host "Ended!"
+}
